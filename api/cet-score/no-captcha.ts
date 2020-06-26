89b2aed01fbdfa7955ba5c23b07f182a67a4485c
Kid <kidonng@gmail.com>
2020-06-26 16:27:10 +0800

    fix: `cet-score` types

--- api/cet-score/no-captcha.ts
+++ api/cet-score/no-captcha.ts
@@ -5,12 +5,12 @@ export default async (req: NowRequest, res: NowResponse) => {
   const { body, headers } = await got(
     'http://cet-bm.neea.cn/Home/VerifyCodeImg',
     {
-      responseType: 'buffer'
+      responseType: 'buffer',
     }
   )
 
   res.status(200)
   res.setHeader('content-type', 'image/jpg')
-  res.setHeader('set-cookie', headers['set-cookie'])
+  res.setHeader('set-cookie', headers['set-cookie']!)
   res.end(body)
 }
